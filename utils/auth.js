d77df0e4d319820c1b727599504c2df917cf9cff
coder0098 <parth.dawda98@gmail.com>
2024-10-03 17:34:10 +0530

    remove selected data and search results from local storage after logout

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,11 @@
 import { supabase } from './supabaseClient';
 
+
 export const logout = async (e) => {
   e.preventDefault(); // Prevent the default link behavior
   const { error } = await supabase.auth.signOut();
+  localStorage.removeItem('searchResults');
+  localStorage.removeItem('selectedData');
   if (error) {
     console.error('Error signing out:', error.message);
   } else {
