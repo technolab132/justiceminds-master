f192c472c9b067bb84c4eb0bede66a2df8ab63ca
coder0098 <parth.dawda98@gmail.com>
2024-10-07 14:35:23 +0530

    cookies cleared after logout

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,21 @@
 import { supabase } from './supabaseClient';
 
+const cookie = require('cookie');
+export const clearCookies = () => {
+  const cookies = cookie.parse(document.cookie);
+  for (const cookieName in cookies) {
+    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;`;
+  }
+};
 
 export const logout = async (e) => {
   e.preventDefault(); // Prevent the default link behavior
   const { error } = await supabase.auth.signOut();
   localStorage.removeItem('searchResults');
   localStorage.removeItem('selectedData');
+  
+  clearCookies(); // Clear cookies on logout
+
   if (error) {
     console.error('Error signing out:', error.message);
   } else {
